fb33836f3d70a61631c67f5d0af14d2ac19724ed
hoangthevuongvn <hoangthevuong.vnn@gmail.com>
2021-12-30 17:14:56 +0700

    freeze requirements

--- app1/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import Employee, Question, Choice
-
-admin.site.register(Question)
-admin.site.register(Choice)
-admin.site.register(Employee)
